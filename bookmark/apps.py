e26e24b7dcb3b133cb75288d8a99c0b2d8c7c13c
apeach9 <apeach_9@naver.com>
2023-01-31 00:04:10 +0900

    Bookmark Service

--- bookmark/apps.py
+++ bookmark/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class BookmarkConfig(AppConfig):
     name = 'bookmark'
+
+
+class UsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'users'
